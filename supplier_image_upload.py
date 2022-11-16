d5e9adb6c56ed2393d088df79fa2562bd24cd4f6
edendir <robbielannon@gmail.com>
2022-11-16 13:33:51 -0600

    supplier_image_upload modified

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -8,6 +8,6 @@ path = "supplier-data/images/"
 images = os.listdir(path)
 
 for image in images:
-  if image.endswith(".jpeg"):
-    with open(path + image, 'rb') as opened:
-      r = requests.post(url, files={'file': opened})
+    if image.endswith(".jpeg"):
+        with open(path + image, 'rb') as opened:
+            r = requests.post(url, files={'file': opened})
