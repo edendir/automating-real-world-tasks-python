51b813bbeab0c2daf4899f337859c4e572eaf9be
edendir <robbielannon@gmail.com>
2022-11-16 14:20:51 -0600

    changeImage updated

--- changeImage.py
+++ changeImage.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
-
-# module imports
-import os
 from PIL import Image
+import os, sys
 
-# variables
-user = os.getenv('USER')
-image_directory = '/home/{}/supplier-data/images'.format(user)
+path = "supplier-data/images/"
+pictures = os.listdir(path)
 
-for image in image_directory:
-    if 'tiff' in image:
-        file_name = os.path.splitext(image)[0]
-        new_file = image_directory + file_name + ".jpg"
-        try:
-            Image.open(new_file).convert("RGB").resize((600, 400)).save(new_file, "JPEG")
-        except IOError:
-            print("cannot convert", image)
+for pic in pictures:
+  if 'tiff' in pic:
+    #grab the picture name without the .tiff extension
+    file_name = os.path.splitext(pic)[0]
+    outfile = "supplier-data/images/" + file_name + ".jpeg"
+    try:
+      Image.open(path + pic).convert("RGB").resize((600,400)).save(outfile,"JPEG")
+    except IOError:
+      print("cannot convert", pic)
